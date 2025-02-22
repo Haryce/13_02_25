7dd50901324dc96fc38964c5173ce6e359d87504
Haryce <148765923+Haryce@users.noreply.github.com>
2025-02-22 16:39:15 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,2 +1,19 @@
-1) Напишите макрос-функцию периметр треугольника
-2) Напишите макрос-функцию длины окружности(используйте макрос-определение для числа ПИ)
+#include <iostream>
+#define perimetr ((a) + (b) + (c)) //1
+#define PI 3.14
+#define circle (2 * PI * (r)) //2
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите 3 стороны треугольника" << endl;
+	double a, b, c;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cout << "Введите радиус круга" << endl;
+	double r;
+	cin >> r;
+	cout << "Периметр треугольника: " << a + b + c << endl;
+	cout << "Длина окружности: " << 2 * PI * r;
+}
